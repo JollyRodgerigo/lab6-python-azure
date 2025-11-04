f0c81db8ef4d6c50f1e44f75c8c491eb2d5ccc39
JollyRodgerigo <harciza539@gmail.com>
2025-11-04 11:12:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello from Azure with Python! V2</h1><p>Виконав ПІБ студента <\p>"
+    return "<h1>Hello from Azure with Python! V2</h1><p>Виконав Полончук Павло Олександрович/Веселий Роджер/Болбочан студента групи КН-22 <\p>"
 
 if __name__ == '__main__':
     app.run()
+
 
